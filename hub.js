1a630060184333570c2fcd88a9ac1191e505f3c8
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-08-15 20:53:38 -0700

    started code

---
+++ hub.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { Server } = require("socket.io");
+
+//socket server instance
+const io = new Server();
+io.listen(3000);
+
+function newMessageHandler(payload) {
+    io.to('diabloFour').emit(payload);
+    console.log(payload);
+}
+
+function joinRoom(payload, socket) {
+  socket.join(payload)
+  console.log(`room ${payload} joined!`);
+}
+
+let userNumber = 0;
+function handleConnection(socket) {
+    userNumber++;
+    console.log("We have a new connection: " + userNumber);
+    socket.on('roomName', (payload) => joinRoom(payload, socket));
+    socket.on("newMessage", newMessageHandler);
+}
+
+function startSocketServer() {
+    console.log("The Socket Server has started!");
+    // connection in a sense is a magic word than knows to react or listen to any client connect made. A socket obj will be passed on connection.
+    io.on("connection", handleConnection);
+}
+
+  module.exports = {
+    startSocketServer,
+    Server,
+  }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# CHATTER
+
+This is a basic chat app server where people can join their favorite room to chat with others about all things pertaining to their favorite topic.
+
+## UML
+
+![diagramofchatter](./img/chatteruml.png)
